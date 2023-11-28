55867cd647855a08d713a5547665ce6c1494df26
roelccentega <ccenteproga@gmail.com>
2023-11-28 09:55:49 -0500

    cambios landing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,22 +28,22 @@
     <link rel="stylesheet" href="{{asset('assets/vendors/animate/custom-animate.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendors/fontawesome/css/all.min.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendors/jarallax/jarallax.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css')}}" />
+    <!-- <link rel="stylesheet" href="{{asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendors/nouislider/nouislider.min.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendors/nouislider/nouislider.pips.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/odometer/odometer.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendors/odometer/odometer.min.css')}}" /> -->
     <link rel="stylesheet" href="{{asset('assets/vendors/swiper/swiper.min.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendors/gardon-icons/style.c')}}ss">
     <link rel="stylesheet" href="{{asset('assets/vendors/tiny-slider/tiny-slider.min.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendors/reey-font/stylesheet.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/alagambe-font/stylesheet.css')}}" />
+    <!-- <link rel="stylesheet" href="{{asset('assets/vendors/alagambe-font/stylesheet.css')}}" /> -->
     <link rel="stylesheet" href="{{asset('assets/vendors/owl-carousel/owl.carousel.min.css')}}" />
     <link rel="stylesheet" href="{{asset('assets/vendors/owl-carousel/owl.theme.default.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/bxslider/jquery.bxslider.css')}}" />
+    <!-- <link rel="stylesheet" href="{{asset('assets/vendors/bxslider/jquery.bxslider.css')}}" /> -->
     <link rel="stylesheet" href="{{asset('assets/vendors/bootstrap-select/css/bootstrap-select.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/vegas/vegas.min.css')}}" />
+    <!-- <link rel="stylesheet" href="{{asset('assets/vendors/vegas/vegas.min.css')}}" /> -->
     <link rel="stylesheet" href="{{asset('assets/vendors/jquery-ui/jquery-ui.css')}}" />
-    <link rel="stylesheet" href="{{asset('assets/vendors/timepicker/timePicker.css')}}" />
+    <!-- <link rel="stylesheet" href="{{asset('assets/vendors/timepicker/timePicker.css')}}" /> -->
     <link rel="stylesheet" href="{{asset('assets/vendors/twenty-twenty/twentytwenty.css')}}" />
 
     <!-- template styles -->
